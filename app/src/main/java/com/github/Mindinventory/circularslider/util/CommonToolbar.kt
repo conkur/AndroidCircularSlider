132ca87b73e223ec956c94f1744903f168f57c94
conkur <kevinjyoungjr@gmail.com>
2023-01-11 09:36:26 -0500

    chore(build): updates target api and moves to material3
    Used this guide to do the material migration:
    https://developer.android.com/jetpack/compose/designsystems/material2-material3

--- app/src/main/java/com/github/Mindinventory/circularslider/util/CommonToolbar.kt
+++ app/src/main/java/com/github/Mindinventory/circularslider/util/CommonToolbar.kt
@@ -3,7 +3,7 @@ package com.github.Mindinventory.circularslider.util
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
